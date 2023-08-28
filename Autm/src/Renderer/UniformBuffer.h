1316ac10fb47d7109144226efda3e4cf9dbc785f
kregerl <loucaskreger@gmail.com>
2023-08-27 20:13:15 -0400

    Replace GLuint's with uint32_t's to be consistent with the rest of the engine

--- Autm/src/Renderer/UniformBuffer.h
+++ Autm/src/Renderer/UniformBuffer.h
@@ -1,16 +1,18 @@
 #ifndef AUTM_UNIFORMBUFFER_H
 #define AUTM_UNIFORMBUFFER_H
 
-#include <glad/glad.h>
+#include "autmpch.h"
 
 class UniformBuffer {
 public:
     UniformBuffer(uint32_t size, uint32_t binding);
+
     ~UniformBuffer();
 
     void set_data(const void* data, uint32_t size, uint32_t offset = 0);
+
 private:
-    GLuint m_renderer_id;
+    uint32_t m_renderer_id;
 };
 
 
